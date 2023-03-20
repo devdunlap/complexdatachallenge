02da269630e6b0d34d93e0b27c5607d730be7e76
devdunlap <108426486+devdunlap@users.noreply.github.com>
2023-03-20 09:38:02 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const companies = [
+    { name: "Money 4 U", industry: "Finance", start: 1981, end: 2004 },
+    { name: "The Clothes Bay", industry: "Retail", start: 1992, end: 2008 },
+    { name: "Luxurious Limousines", industry: "Auto", start: 1999, end: 2007 },
+    { name: "Sunglasses House", industry: "Retail", start: 1989, end: 2010 },
+    { name: "Vantage Solutions", industry: "Technology", start: 2009, end: 2014 },
+    { name: "Money Banking", industry: "Finance", start: 1987, end: 2010 },
+    { name: "Triple Motors", industry: "Auto", start: 1986, end: 1996 },
+    { name: "Mech Smart", industry: "Technology", start: 2011, end: 2016 },
+    { name: "Ice-Cream Lettuce", industry: "Retail", start: 1981, end: 1989 },
+  ];
+// when the button is clicked this funtion occurs
+  onbuttonclick = (container) => {array.forEach(companies=> {
+     console.log,companies[0]
+  }); }
+//   I want a for each statement that loops through the companies array and prints out the name of each company
